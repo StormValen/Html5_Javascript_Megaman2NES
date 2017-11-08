61e51f397cd62e619f522d318f61e3f2505e68be
Marc Segarra <marcssegarra@gmail.com>
2017-11-09 00:23:11 +0100

    collision staris yes but overlap does not

--- js/scene_Game.js
+++ js/scene_Game.js
@@ -14,9 +14,6 @@ MegamanGame.scene_Game= {
         this.load.image('MegamanTileset','img/MegamanTileset.png');
         this.load.image('basic','img/fondonegro.png');
         
-        //this.load.spritesheet('megaman_idle_run','img/megaman_idle_run.png',24,24);
-        //this.load.spritesheet('megaman_run','img/megaman_run.png',24,24);
-        
         this.game.load.atlas('megaman_sprites', 'img/sprites.png', 'img/sprites.json');
         
     },
@@ -31,15 +28,9 @@ MegamanGame.scene_Game= {
         this.blockedDoor = this.map.createLayer('BlockedDoor');
         this.map.createLayer('Background'); 
         
-        this.map.setCollisionBetween(0,100,true,'Terrain');
+        this.map.setCollisionBetween(0,100,true,'Terrain',true);
+        this.map.setCollisionBetween(0,100,true,'Stairs',true);    
         
-        
-        /*
-        this.megaman = this.game.add.sprite(100,80,'megaman_idle_run',0);
-        this.megaman.anchor.setTo(0.5);
-        this.megaman.animations.add("idle",[0,1,2],10,true);
-        this.megaman.animations.add("run",[3,4,5],10,true);
-        */
         this.megaman = this.game.add.sprite(100,80,'megaman_sprites');
         this.megaman.anchor.setTo(0.5);
         this.megaman.animations.add('idle',Phaser.Animation.generateFrameNames('idle', 1, 3), 10, true);
@@ -47,9 +38,12 @@ MegamanGame.scene_Game= {
         this.megaman.animations.add('jump',Phaser.Animation.generateFrameNames('jump',1,1), 10, true);
         this.megaman.animations.add('shoot_idle',Phaser.Animation.generateFrameNames('shoot_idle',1,1), 10, true);
         this.megaman.animations.add('shoot_run',Phaser.Animation.generateFrameNames('shoot_run',1,3), 10, true);
+        this.megaman.animations.add('shoot_air',Phaser.Animation.generateFrameNames('shoot_air',1,1), 10, true);
+        this.megaman.animations.add('stair',Phaser.Animation.generateFrameNames('stair',1,2), 10, true);
         
         this.game.physics.arcade.enable(this.megaman);
-        this.megaman.body.setSize(15,24);
+        this.megaman.body.collideWorldBounds = true;
+        this.megaman.body.setSize(14,24);
         
         
         this.cursors = this.game.input.keyboard.createCursorKeys();
@@ -60,30 +54,26 @@ MegamanGame.scene_Game= {
     },
     
     update:function(){
-       this.game.physics.arcade.collide(this.megaman,this.terrain);
+        this.game.physics.arcade.collide(this.megaman,this.terrain);
+        this.game.physics.arcade.overlap(this.megaman,this.stairs, this.hasCollidedWithStairs, null, this);
         
+        this.megaman.body.applyGravity = true;
         this.megaman.body.velocity.x = 0;
         
-        //ACCIONES POR TECLAS
+        //ACCIONES POR TECLAS MEGAMAN
         if(this.cursors.left.isDown)
         {
             this.megaman.body.velocity.x = -gameOptions.megamanSpeed;
             
             if(this.x.isDown && this.megaman.body.blocked.down)
             {
-                    if(this.megaman.body.velocity.x < 0)
-                    {
-                        this.megaman.animations.play("shoot_run");
-                        this.megaman.scale.x = 1;   
-                    }
+                this.megaman.animations.play("shoot_run");    
             }
-            
-            else if(this.megaman.body.velocity.y == 0){
-                this.megaman.animations.play("run");
-                this.megaman.scale.x = 1;   
+            else if(this.megaman.body.blocked.down)
+            {
+                this.megaman.animations.play("run"); 
             }
-            
-          
+            this.megaman.scale.x = 1; 
         }
         
         else if(this.cursors.right.isDown)
@@ -92,20 +82,16 @@ MegamanGame.scene_Game= {
             
             if(this.x.isDown && this.megaman.body.blocked.down)
             {
-                if(this.megaman.body.velocity.x > 0)
-                {
-                    this.megaman.animations.play("shoot_run");
-                    this.megaman.scale.x = -1;   
-                }
+                this.megaman.animations.play("shoot_run");   
             }
-            else if(this.megaman.body.velocity.y == 0)
+            else if(this.megaman.body.blocked.down)
             {
                 this.megaman.animations.play("run");
-                this.megaman.scale.x = -1; 
             }
-             
+            this.megaman.scale.x = -1;  
         }
-        else if(this.megaman.body.velocity.y == 0 && this.megaman.body.velocity.x == 0){
+        
+        else if(this.megaman.body.blocked.down && this.megaman.body.velocity.x == 0){
              
             if(this.x.isDown && this.megaman.body.blocked.down){
                  this.megaman.animations.play("shoot_idle");
@@ -116,11 +102,35 @@ MegamanGame.scene_Game= {
             }
         }
         
-       if(this.z.isDown && this.megaman.body.blocked.down && this.z.downDuration(250)){
-            this.megaman.body.velocity.y = -gameOptions.megamanJump;
-           this.megaman.animations.play("jump");
-        }
+       if(this.z.isDown && this.megaman.body.blocked.down && this.z.downDuration(250))
+       {
+           this.megaman.body.velocity.y = -gameOptions.megamanJump;
+           this.megaman.animations.play("jump");  
+       }/*
+       if(this.x.isDown && !this.megaman.body.blocked.down)
+       {
+           this.megaman.animations.play("shoot_air");
+       }*/
         
 
+    },
+    
+    hasCollidedWithStairs:function(obj1, obj2){
+        console.log('yes');
+        this.megaman.body.applyGravity = false;
+        
+        if(this.cursors.up.isDown)
+        {
+            this.megaman.body.velocity.y = -gameOptions.megamanSpeed;
+            this.megaman.animations.play("stair");
+            this.megaman.scale.x = 1; 
+        }
+        
+        else if(this.cursors.down.isDown)
+        {
+            this.megaman.body.velocity.y = +gameOptions.megamanSpeed;
+            this.megaman.animations.play("stair");
+            this.megaman.scale.x = 1; 
+        }
     }
-}
+};
